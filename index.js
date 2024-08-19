1c7601d3708fdb65fffa41d531f01e171300e10d
Danilo Casim Jr. <danilocasim174@gmail.com>
2024-08-19 16:21:59 +0800

    Add alert for every declare of win/lose/draw
    Add alert to identify the scores of both user and computer also to tell the result of fight

--- index.js
+++ index.js
@@ -16,34 +16,61 @@ function playRound(humanChoice, computerChoice) {
     case "ROCK":
       if (computerChoice === "ROCK") {
         console.log("Draw!");
+        alert(`Draw!
+        Your Score: ${humanScore}
+        Computer Score ${computerScore}`);
       } else if (computerChoice === "PAPER") {
         console.log("You Lose! Paper beats Rock");
+        alert(`You Lose! Paper beats Rock
+        Your Score: ${humanScore}
+        Computer Score ${computerScore}`);
         computerScore++;
       } else if (computerChoice === "SCISSORS") {
         console.log("You Win! Rock beats Scissors");
+        alert(`You Win! Rocks beats Scissors
+        Your Score: ${humanScore}
+        Computer Score ${computerScore}`);
         humanScore++;
       }
       break;
     case "PAPER":
       if (computerChoice === "ROCK") {
         console.log("You Win! Paper beats Rock");
+        alert(`You Win! Paper beats Rock
+        Your Score: ${humanScore}
+        Computer Score ${computerScore}`);
         humanScore++;
       } else if (computerChoice === "PAPER") {
         console.log("Draw!");
+        alert(`Draw!
+        Your Score: ${humanScore}
+        Computer Score ${computerScore}`);
       } else if (computerChoice === "SCISSORS") {
         console.log("You Lose! Scissors beats Paper");
+        alert(`You Lose! Scossors beats Paper
+        Your Score: ${humanScore}
+        Computer Score ${computerScore}`);
         computerScore++;
       }
       break;
     case "SCISSORS":
       if (computerChoice === "ROCK") {
         console.log("You Lose! Rock beats Scissors");
+        alert(`You Lose! Rock beats Scissors
+        Your Score: ${humanScore}
+        Computer Score ${computerScore}`);
         computerScore++;
       } else if (computerChoice === "PAPER") {
         console.log("You Win! Scissors beats Paper");
+        alert(`You Win! Scissors beats Paper
+        Your Score: ${humanScore}
+        Computer Score ${computerScore}`);
         humanScore++;
       } else if (computerChoice === "SCISSORS") {
         console.log("Draw!");
+        alert(`Draw!
+        Your Score: ${humanScore}
+        Computer Score ${computerScore}`);
       }
       break;
   }
