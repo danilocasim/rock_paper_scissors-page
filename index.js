4eaaf2624a34fbea702482f4e58d70baaf38284f
Danilo Casim Jr. <danilocasim174@gmail.com>
2024-08-19 16:32:50 +0800

    Adjust the alignment of all alert

--- index.js
+++ index.js
@@ -17,66 +17,71 @@ function playRound(humanChoice, computerChoice) {
       if (computerChoice === "ROCK") {
         console.log("Draw!");
         alert(`Draw!
-        Your Score: ${humanScore}
-        Computer Score ${computerScore}`);
+Your Score: ${humanScore}
+Computer Score ${computerScore}`);
       } else if (computerChoice === "PAPER") {
         console.log("You Lose! Paper beats Rock");
         computerScore++;
         alert(`You Lose! Paper beats Rock
-        Your Score: ${humanScore}
-        Computer Score ${computerScore}`);
+Your Score: ${humanScore}
+Computer Score ${computerScore}`);
       } else if (computerChoice === "SCISSORS") {
         console.log("You Win! Rock beats Scissors");
         humanScore++;
 
         alert(`You Win! Rocks beats Scissors
-        Your Score: ${humanScore}
-        Computer Score ${computerScore}`);
+Your Score: ${humanScore}
+Computer Score ${computerScore}`);
       }
+
       break;
+
     case "PAPER":
       if (computerChoice === "ROCK") {
         console.log("You Win! Paper beats Rock");
         humanScore++;
 
         alert(`You Win! Paper beats Rock
-        Your Score: ${humanScore}
-        Computer Score ${computerScore}`);
+Your Score: ${humanScore}
+Computer Score ${computerScore}`);
       } else if (computerChoice === "PAPER") {
         console.log("Draw!");
         alert(`Draw!
-        Your Score: ${humanScore}
-        Computer Score ${computerScore}`);
+Your Score: ${humanScore}
+Computer Score ${computerScore}`);
       } else if (computerChoice === "SCISSORS") {
         console.log("You Lose! Scissors beats Paper");
         computerScore++;
 
         alert(`You Lose! Scossors beats Paper
-        Your Score: ${humanScore}
-        Computer Score ${computerScore}`);
+Your Score: ${humanScore}
+Computer Score ${computerScore}`);
       }
+
       break;
+
     case "SCISSORS":
       if (computerChoice === "ROCK") {
         console.log("You Lose! Rock beats Scissors");
         computerScore++;
 
         alert(`You Lose! Rock beats Scissors
-        Your Score: ${humanScore}
-        Computer Score ${computerScore}`);
+Your Score: ${humanScore}
+Computer Score ${computerScore}`);
       } else if (computerChoice === "PAPER") {
         console.log("You Win! Scissors beats Paper");
         humanScore++;
 
         alert(`You Win! Scissors beats Paper
-        Your Score: ${humanScore}
-        Computer Score ${computerScore}`);
+Your Score: ${humanScore}
+Computer Score ${computerScore}`);
       } else if (computerChoice === "SCISSORS") {
         console.log("Draw!");
         alert(`Draw!
-        Your Score: ${humanScore}
-        Computer Score ${computerScore}`);
+Your Score: ${humanScore}
+Computer Score ${computerScore}`);
       }
+
       break;
   }
 }
